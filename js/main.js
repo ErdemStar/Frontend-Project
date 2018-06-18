a4996db18ad5a5f7046478e1b38f146cd1c428ae
Erdem STAR <erdem-yildiz@windowslive.com>
2018-06-18 14:00:51 +0300

    Genel olarak tasarım şekillendi.Slider ve kayan yazı düzenlendi

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Alex Smith.", "Designer.", "Erdem.", "Veli.", "Lerim"],
       typeSpeed: 100,
       loop: true,
     });
